a74b5bc4e691ba555615267cf42029e5c6c4e259
Patrick Nielsen <ptnielsen55@gmail.com>
2015-05-20 11:06:55 -0400

    Completed Daily Challenge - Caffeine

--- coffee.rb
+++ coffee.rb
@@ -1,3 +1,18 @@
 class Coffee
-  # Add code here
+  def initialize(type)
+  	@full = true
+  	@sips = 3
+  end
+
+  def full?
+  	@sips == 3
+  end
+
+  def take_sip
+  	@sips -= 1
+  end
+
+  def empty?
+  	@sips.zero?
+  end
 end
--- human.rb
+++ human.rb
@@ -1,3 +1,30 @@
 class Human
-  # Add code here
+  def initialize(name)
+  	@alertness = 0
+  end
+
+  def alertness
+  	@alertness
+  end
+
+  def buy coffee
+  	@my_coffee = coffee
+  end
+
+  def drink!
+  	@alertness += 0.33
+  	@my_coffee.take_sip
+  end
+
+  def has_coffee?
+  	if @my_coffee.nil?
+  		false
+  	else
+  		true
+  	end
+  end
+
+  def needs_coffee?
+  	not has_coffee?
+  end
 end
